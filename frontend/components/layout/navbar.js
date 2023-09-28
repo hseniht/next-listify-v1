8d9be150a234cc1c2cc81945d3e31a759b7ff515
hseniht <thinesh_kumar_mu@hotmail.com>
2023-09-28 23:24:02 +0800

    Add some pages for navigation

---
+++ frontend/components/layout/navbar.js
@@ -0,0 +1,26 @@
+import Link from "next/link";
+
+const Navbar = () => {
+  return (
+    <header id="mainHeader">
+      <div id="mainNavigation" className="container">
+        <div className="brand-container">Logo</div>
+        <nav className="main-menu">
+          <ul>
+            <li className="menu-item">
+              <Link href={"/"}>Home</Link>
+            </li>
+            <li className="menu-item">
+              <Link href={"/list"}>List</Link>
+            </li>
+            <li className="menu-item">
+              <Link href={"/contact"}>Contact</Link>
+            </li>
+          </ul>
+        </nav>
+      </div>
+    </header>
+  );
+};
+
+export default Navbar;
