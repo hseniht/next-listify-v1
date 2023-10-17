9263e93b7cfae58f7bc2e867a3787c747764cc0c
hseniht <thinesh_kumar_mu@hotmail.com>
2023-10-18 00:09:46 +0800

    Add user registration static method & hashing
    - Implemented signup static method on userSchema
    - Checks for existing email addresses and throws an error if found
    - Safely stores hashed passwords using bcrypt
    
    This commit adds the user registration functionality, ensuring email
    uniqueness and password security.

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -1,4 +1,5 @@
 const mongoose = require("mongoose");
+const bcrypt = require("bcrypt");
 
 const Schema = mongoose.Schema;
 
@@ -13,5 +14,23 @@ const userSchema = new Schema({
     require: true,
   },
 });
+
+//static signup method
+userSchema.statics.signup = async function (email, password) {
+  // Some Note: When you use `this` within a method of an object, it refers to the object itself.
+  // so `this` keyword refers to the model. In this case : "User"
+  const exist = await this.findOne({ email });
+
+  if (exist) {
+    throw Error("Email already in use");
+  }
+  //salt - random characters string that is added to a user's password before hash
+  const salt = await bcrypt.genSalt(10); //this will take time. Higher value, the longer it takes
+  const hash = await bcrypt.hash(password, salt);
+
+  const user = await this.create({ email, password: hash });
+
+  return user;
+};
 
 module.exports = mongoose.model("User", userSchema);
