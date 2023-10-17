4f1aab8b2779bbf2a3e827d7364bb13724234caa
hseniht <thinesh_kumar_mu@hotmail.com>
2023-10-18 00:41:43 +0800

    chore: Add validation for user signup
    - also fixed typo 'require' on userSchema

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -1,22 +1,37 @@
 const mongoose = require("mongoose");
 const bcrypt = require("bcrypt");
+const validator = require("validator");
 
 const Schema = mongoose.Schema;
 
 const userSchema = new Schema({
   email: {
     type: String,
-    require: true,
+    required: true,
     unique: true, //prevents duplication from mongoose
   },
   password: {
     type: String,
-    require: true,
+    required: true,
   },
 });
 
 //static signup method
 userSchema.statics.signup = async function (email, password) {
+  // validation
+  if (!email || !password) {
+    throw Error("All fields must be filled");
+  }
+
+  //if email not valid
+  if (!validator.isEmail(email)) {
+    throw Error("Email is not valid");
+  }
+
+  if (!validator.isStrongPassword(password)) {
+    throw Error("Password is not strong enough");
+  }
+
   // Some Note: When you use `this` within a method of an object, it refers to the object itself.
   // so `this` keyword refers to the model. In this case : "User"
   const exist = await this.findOne({ email });
