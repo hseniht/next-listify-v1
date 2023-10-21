9e81b37a1c7bd1bc914f648fd2d9d575ffea049e
hseniht <thinesh_kumar_mu@hotmail.com>
2023-10-21 16:07:02 +0800

    login hook
    Similiar to signup hook (refer commit  ef2e5b387eb905ead0480b1a6e7947e95f39f4f31)

--- frontend/pages/login.js
+++ frontend/pages/login.js
@@ -1,13 +1,15 @@
 import { useState } from "react";
+import { useLogin } from "../hooks/useLogin";
 
 const Login = () => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  const { login, error, isLoading } = useLogin();
 
   const handleSubmit = async (e) => {
     e.preventDefault();
 
-    console.log("login: ", email, password);
+    await login(email, password);
   };
 
   return (
@@ -27,7 +29,8 @@ const Login = () => {
         value={password}
       ></input>
 
-      <button>Login</button>
+      <button disabled={isLoading}>Login</button>
+      {error && <p className="error">{error}</p>}
     </form>
   );
 };
