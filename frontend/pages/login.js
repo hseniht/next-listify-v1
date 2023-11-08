8f5bf8ced7169d3deff82f771ad8f48b7337d92e
hseniht <thinesh_kumar_mu@hotmail.com>
2023-11-08 23:09:05 +0800

    Rework Login Form with AntDesign
    'useState' no longer used for form inputs. It is handled by 'antd' form
    components.
    
    Antd's component handles validation, input values and form submitions
    either success or error.
    
    Also added stylings for Login page.

--- frontend/pages/login.js
+++ frontend/pages/login.js
@@ -2,8 +2,25 @@ import { useState, useEffect } from "react";
 import { useLogin } from "../hooks/useLogin";
 import { useAuthContext } from "../hooks/useAuthContext";
 import { useRouter } from "next/router";
+import { PageWrapper } from "../components/ui/layout";
+import {
+  Button,
+  Card,
+  Checkbox,
+  Col,
+  Flex,
+  Row,
+  Layout,
+  Form,
+  Input,
+  Typography,
+} from "antd";
+const { Text } = Typography;
+
+import styles from "../styles/layout.module.css";
 
 const Login = () => {
+  //form input states no longer needed as "AntD Form UI" handles it.
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const { login, error, isLoading } = useLogin();
@@ -16,32 +33,100 @@ const Login = () => {
     }
   }, [user]);
 
-  const handleSubmit = async (e) => {
-    e.preventDefault();
+  // const handleSubmit = async (e) => {
+  //   e.preventDefault();
 
+  //   await login(email, password);
+  // };
+
+  const onFinish = async (values) => {
+    const { email, password } = values;
     await login(email, password);
+    console.log("login success");
+  };
+  const onFinishFailed = (errorInfo) => {
+    console.log("Login failed:", errorInfo);
   };
 
   return (
-    <form className="login" onSubmit={handleSubmit}>
-      <h3>Login</h3>
+    <PageWrapper
+      id={"loginPage"}
+      // className={"page__wrapper--login"}
+      className={`${styles["page__wrapper--login"]}`}
+    >
+      <Card>
+        <h3>Login</h3>
+        <Form
+          name="login"
+          labelCol={{ span: 8 }}
+          wrapperCol={{ span: 16 }}
+          // style={{
+          //   maxWidth: 600,
+          // }}
+          // initialValues={{
+          //   remember: true,
+          // }}
+          onFinish={onFinish}
+          // onFinish={onFinish}
+          onFinishFailed={onFinishFailed}
+          autoComplete="off"
+        >
+          <Form.Item
+            label="Email"
+            name="email"
+            initialValue={""}
+            rules={[
+              {
+                type: "email",
+                required: true,
+                // message: "Please input your username!",
+              },
+            ]}
+          >
+            <Input />
+          </Form.Item>
 
-      <label>Email:</label>
-      <input
-        type="email"
-        onChange={(e) => setEmail(e.target.value)}
-        value={email}
-      ></input>
-      <label>Password:</label>
-      <input
-        type="password"
-        onChange={(e) => setPassword(e.target.value)}
-        value={password}
-      ></input>
+          <Form.Item
+            label="Password"
+            name="password"
+            initialValue={""}
+            rules={[
+              {
+                required: true,
+                // message: "Please input your password!",
+              },
+            ]}
+          >
+            <Input.Password />
+          </Form.Item>
 
-      <button disabled={isLoading}>Login</button>
-      {error && <p className="error">{error}</p>}
-    </form>
+          {/* <Form.Item
+            name="remember"
+            valuePropName="checked"
+            wrapperCol={{
+              offset: 8,
+              span: 16,
+            }}
+          >
+            <Checkbox>Remember me</Checkbox>
+          </Form.Item> */}
+
+          <Form.Item
+            wrapperCol={{
+              offset: 8,
+              span: 16,
+            }}
+          >
+            {/* Todo UI: Popup the error */}
+            <Col>{error && <Text type="danger" style={{fontSize: "12px", }}>{error}</Text>}</Col>
+            <Button type="primary" htmlType="submit">
+              Submit
+            </Button>
+          </Form.Item>
+        </Form>
+      </Card>
+    </PageWrapper>
+    //</Layout>
   );
 };
 
