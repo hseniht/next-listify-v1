55233c22edcbc2efec81840823d4104c7a04fef5
hseniht <thinesh_kumar_mu@hotmail.com>
2023-12-07 21:46:05 +0800

    chore: Fix unescaped single quote in Paragraph
    Resolves an ESLint error related to the 'react/no-unescaped-entities'
    rule by escaping a single quote in the text content of the Paragraph
    component in pages/index.js. This ensures proper rendering and improves
    code quality.

--- frontend/pages/index.js
+++ frontend/pages/index.js
@@ -5,6 +5,15 @@ import { Button, Space, Layout, Typography } from "antd";
 const { Title, Paragraph, Text } = Typography;
 import { PageWrapper } from "../components/ui/layout";
 
+const descriptionText = `
+  Transform your ideas into organized notes with Listify! Whether you're
+  jotting down thoughts, creating to-do lists, or brainstorming, Listify
+  is your ultimate tool for capturing and managing your thoughts. Say
+  goodbye to scattered notes and hello to a streamlined note-taking
+  experience. Start using Listify today and bring order to your
+  creativity!
+`;
+
 export default function Home() {
   return (
     <PageWrapper
@@ -12,21 +21,14 @@ export default function Home() {
       className={`${styles["page__wrapper--hero"]} ${styles["wh-100"]}`}
     >
       <Typography direction="vertical" size="middle">
-        <Title>Welcome to Listify!</Title>
-        <Title level={3}>Get Ready to Organize and Capture Ideas</Title>
-        <Paragraph>
-          Transform your ideas into organized notes with Listify! Whether you're
-          jotting down thoughts, creating to-do lists, or brainstorming, Listify
-          is your ultimate tool for capturing and managing your thoughts. Say
-          goodbye to scattered notes and hello to a streamlined note-taking
-          experience. Start using Listify today and bring order to your
-          creativity!
-        </Paragraph>
+        <Title>{"Welcome to Listify!"}</Title>
+        <Title level={3}>{"Get Ready to Organize and Capture Ideas"}</Title>
+        <Paragraph>{descriptionText}</Paragraph>
         <br />
         <div className="menu-item">
           <Link href={"list"}>
             <Space>
-              <span>Start Taking Notes</span>
+              <span>{"Start Taking Notes"}</span>
               <Button
                 type="primary"
                 shape="circle"
