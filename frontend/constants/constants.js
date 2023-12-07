fc55fb580b2f564ceee6271f9ce8c4a2e5f0bdb4
hseniht <thinesh_kumar_mu@hotmail.com>
2023-12-07 22:40:36 +0800

    feat: Configure API URLs dynamically based on env
    Refactor the API configuration to use dynamic URLs based on
    the environment. During local development, the URLs include
    'http://localhost:3001', and in production, they become relative to the
    deployed domain. This ensures seamless API calls in different
    environments.

--- frontend/constants/constants.js
+++ frontend/constants/constants.js
@@ -1,6 +1,9 @@
 //url variables
-export const TODOS_API_URL = "http://localhost:3001/api/notepad/todos";
-export const TAGS_API_URL = "http://localhost:3001/api/notepad/tags";
-export const SIGNUP_API_URL = "http://localhost:3001/api/notepad/signup";
-export const LOGIN_API_URL = "http://localhost:3001/api/notepad/login";
+const baseURL =
+  process.env.NODE_ENV === "production" ? "" : "http://localhost:3001";
+
+export const TODOS_API_URL = `${baseURL}/api/notepad/todos`;
+export const TAGS_API_URL = `${baseURL}/api/notepad/tags`;
+export const SIGNUP_API_URL = `${baseURL}/api/notepad/signup`;
+export const LOGIN_API_URL = `${baseURL}/api/notepad/login`;
 // Todo try Proxy
