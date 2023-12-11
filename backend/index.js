5ebcd77dd765e771e4a10be9bdd3382f40a3c3f9
hseniht <thinesh_kumar_mu@hotmail.com>
2023-12-11 22:43:06 +0800

    Remove old localhost url on allowed CORS

--- backend/index.js
+++ backend/index.js
@@ -13,28 +13,11 @@ app.use(express.json());
 
 // Enable CORS
 app.use((req, res, next) => {
-  const allowedOrigins = ["http://localhost:3000", process.env.FRONTEND_URL];
-  const origin = req.headers.origin;
-
-  if (allowedOrigins.includes(origin)) {
-    res.setHeader("Access-Control-Allow-Origin", origin);
-  }
-
-  res.setHeader(
-    "Access-Control-Allow-Origin",
-    "http://localhost:3000",
-    process.env.FRONTEND_URL
-  );
+  res.setHeader("Access-Control-Allow-Origin", "https://next-listify-v1-api.vercel.app");
   res.setHeader("Access-Control-Allow-Methods", "GET, PATCH, POST, DELETE");
   res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
   res.setHeader("Access-Control-Allow-Credentials", true); // If you need credentials
-
-  // Handle preflight requests
-  if (req.method === "OPTIONS") {
-    res.status(200).end();
-  } else {
-    next(); // Continue with the main request
-  }
+  next(); //to continue run next function
 });
 
 // Handle preflight requests (OPTIONS)
